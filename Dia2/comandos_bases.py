16c6b0136efcc92730c582c54cf8cd4bea141099
JRobert018 <jbetinho31@gmail.com>
2024-08-20 20:35:29 -0300

    assuntos sobre o 2 dia da fábrica.

---
+++ Dia2/comandos_bases.py
@@ -0,0 +1,27 @@
+#inteios
+inteiro = int(10)
+
+#flutuante
+fl = float(10.5)
+#lista
+lista = [1,2,3,4,5]
+#tupla
+tupla = (1,2,3,4,5)
+#string 
+palavra = 'oi'
+#booleano
+bo = True 
+false = False
+
+#dicionario 
+dicionario = {'chave':'valor'}
+print(dicionario)
+dicionario['chave2'] = 'valor2'
+print(dicionario)
+
+chave_dois = dicionario.get('chave2')
+print(chave_dois)
+
+#constante
+CONSTANT = 10
+print(CONSTANT)
